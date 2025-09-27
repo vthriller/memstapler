ef5895dd5ff459aa2164ebcafbc009b877b50880
vthriller <unixway.drive+git@gmail.com>
2025-09-27 18:27:02 +0300

    (reindent)

--- src/main.rs
+++ src/main.rs
@@ -27,68 +27,68 @@ fn main() {
 	});
 
 	loop {
-	let mut files = HashSet::new();
-	let proc = unwrap_ok_or!(read_dir("/proc/"), err, {
-		eprintln!("/proc: cannot open: {}", err);
-		return;
-	});
-	for f in proc {
-		let f = unwrap_ok_or!(f, err, {
-			eprintln!("/proc: while scanning: {}", err);
-			continue;
+		let mut files = HashSet::new();
+		let proc = unwrap_ok_or!(read_dir("/proc/"), err, {
+			eprintln!("/proc: cannot open: {}", err);
+			return;
 		});
-
-		let pid = f.file_name();
-		// not a number in any case, don't bother spamming stderr about that
-		let pid = unwrap_some_or!(pid.to_str(), continue);
-		let _pid: usize = unwrap_ok_or!(pid.parse(), _, continue);
-
-		let f = f.path();
-		let maps = unwrap_ok_or!(File::open(f.join("maps")), err, {
-			eprintln!("{}/maps: failed to read: {}", f.to_string_lossy(), err);
-			continue;
-		});
-		let maps = BufReader::new(maps);
-		for line in maps.lines() {
-			let line = unwrap_ok_or!(line, err, {
-				eprintln!("{}/maps: while reading maps: {}", f.to_string_lossy(), err);
+		for f in proc {
+			let f = unwrap_ok_or!(f, err, {
+				eprintln!("/proc: while scanning: {}", err);
 				continue;
 			});
-			// "7fbe11954000-7fbe11978000 r--p 00000000 fd:00 27526017                   /usr/lib64/libc.so.6"
-			let mut l = line.splitn(6, " ");
-			l.next();
-			let perms = unwrap_some_or!(l.next(), {
-				eprintln!("{}/maps: malformed line: {}", f.to_string_lossy(), line);
+
+			let pid = f.file_name();
+			// not a number in any case, don't bother spamming stderr about that
+			let pid = unwrap_some_or!(pid.to_str(), continue);
+			let _pid: usize = unwrap_ok_or!(pid.parse(), _, continue);
+
+			let f = f.path();
+			let maps = unwrap_ok_or!(File::open(f.join("maps")), err, {
+				eprintln!("{}/maps: failed to read: {}", f.to_string_lossy(), err);
 				continue;
 			});
-			if ! perms.contains('x') {
-				continue;
+			let maps = BufReader::new(maps);
+			for line in maps.lines() {
+				let line = unwrap_ok_or!(line, err, {
+					eprintln!("{}/maps: while reading maps: {}", f.to_string_lossy(), err);
+					continue;
+				});
+				// "7fbe11954000-7fbe11978000 r--p 00000000 fd:00 27526017                   /usr/lib64/libc.so.6"
+				let mut l = line.splitn(6, " ");
+				l.next();
+				let perms = unwrap_some_or!(l.next(), {
+					eprintln!("{}/maps: malformed line: {}", f.to_string_lossy(), line);
+					continue;
+				});
+				if ! perms.contains('x') {
+					continue;
+				}
+				l.next();
+				l.next();
+				l.next();
+				let path = unwrap_some_or!(l.next(), {
+					eprintln!("{}/maps: malformed line: {}", f.to_string_lossy(), line);
+					continue;
+				}).trim_start();
+				if ! path.starts_with('/') {
+					// e.g. "[vdso]" or "" for anon mappings
+					continue;
+				}
+				files.insert(path.to_string());
 			}
-			l.next();
-			l.next();
-			l.next();
-			let path = unwrap_some_or!(l.next(), {
-				eprintln!("{}/maps: malformed line: {}", f.to_string_lossy(), line);
-				continue;
-			}).trim_start();
-			if ! path.starts_with('/') {
-				// e.g. "[vdso]" or "" for anon mappings
-				continue;
+		}
+
+		for f in files {
+			if stdin.write(f.as_bytes()).is_err() {
+				break;
 			}
-			files.insert(path.to_string());
+			if stdin.write(b"\n").is_err() {
+				break;
+			}
 		}
-	}
 
-	for f in files {
-		if stdin.write(f.as_bytes()).is_err() {
-			break;
-		}
-		if stdin.write(b"\n").is_err() {
-			break;
-		}
-	}
-
-	std::thread::sleep(std::time::Duration::new(5, 0));
+		std::thread::sleep(std::time::Duration::new(5, 0));
 	}
 }
 
@@ -133,14 +133,14 @@ fn locker() {
 			).context("mmap") })
 			;
 		match map {
-		Ok(_) => {
-			eprintln!("added {}", &fname);
-			maps.insert(fname.to_string());
-		},
-		Err(err) => {
-			eprintln!("{}: {:#}", &fname, err);
-			continue;
-		}
+			Ok(_) => {
+				eprintln!("added {}", &fname);
+				maps.insert(fname.to_string());
+			},
+			Err(err) => {
+				eprintln!("{}: {:#}", &fname, err);
+				continue;
+			}
 		}
 	}
 }
